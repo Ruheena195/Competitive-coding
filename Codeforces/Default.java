7b4a062a98098c89219a7a5f3057e2e275ba461e
Ruheena195 <123730414+Ruheena195@users.noreply.github.com>
2023-01-27 09:47:15 +0000

    default file created

--- Codeforces/Default.java
+++ Codeforces/Default.java
@@ -1,4 +1,7 @@
-public static void main(String[] args)
-{
-  System.out.println("Hello World");
+class Default{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+  }
 }
+
